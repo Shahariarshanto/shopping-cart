d48f28905ebb3082a1b11e57cbaa48c7e0d062c4
shahariarshanto <shahariarshanto@gmail.com>
2023-02-15 09:49:44 -0500

    updated

--- scripts/app.js
+++ scripts/app.js
@@ -17,10 +17,7 @@ const updateInputValue = (product_btn, inputCount, priceText, price) =>{
     totalPrice = valueCount * price;
     document.getElementById(inputCount).value = valueCount;
     document.getElementById(priceText).innerText = totalPrice;
-
-
-
-    
+    updatePrice()
 }
 // ! _________________________________________________________________
 
@@ -35,3 +32,30 @@ document.getElementById("case-plus").addEventListener("click",()=> updateInputVa
 
 // case-minus
 document.getElementById("case-minus").addEventListener("click",()=> updateInputValue(false,"case-count","case-total-price",59));
+
+
+const updatePrice = ()=>{
+
+
+
+    const phoneTotalPrice = parseFloat(document.getElementById("phone-total-price").innerText) ;
+
+    const caseTotalPrice = parseFloat(document.getElementById("case-total-price").innerText) ;
+
+
+    const subtotalPrice = phoneTotalPrice + caseTotalPrice;
+    document.getElementById("subtotal").innerText = subtotalPrice;
+
+    const taxPrice = parseFloat((subtotalPrice * 0.1).toFixed(2));
+    document.getElementById("tax").innerText = taxPrice;
+
+    const totalPrice = (phoneTotalPrice + caseTotalPrice + taxPrice ).toFixed(2) ;
+
+    console.log(totalPrice);
+    document.getElementById("total").innerText = totalPrice;
+
+
+
+    
+
+}
